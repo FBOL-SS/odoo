28e04668c83b1fc6165bb261e65490ddbec4587c
FBOL-SS <67643371+FBOL-SS@users.noreply.github.com>
2021-11-11 16:49:36 -0600

    Update res_partner.py

--- addons/l10n_co/models/res_partner.py
+++ addons/l10n_co/models/res_partner.py
@@ -10,11 +10,11 @@ class ResPartner(models.Model):
     def check_vat(self):
         # check_vat is implemented by base_vat which this localization
         # doesn't directly depend on. It is however automatically
-        # installed for Colombia.
+        # installed for Honduras.
         if self.sudo().env.ref('base.module_base_vat').state == 'installed':
-            # don't check Colombian partners unless they have RUT (= Colombian VAT) set as document type
-            self = self.filtered(lambda partner: partner.country_id.code != "CO" or\
-                                                 partner.l10n_latam_identification_type_id.l10n_co_document_code == 'rut')
+            # don't check Honduras partners unless they have RTN (= Honduran VAT) set as document type
+            self = self.filtered(lambda partner: partner.country_id.code != "HN" or\
+                                                 partner.l10n_latam_identification_type_id.l10n_co_document_code == 'rtn')
             return super(ResPartner, self).check_vat()
         else:
             return True
